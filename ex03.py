766d30ee1405080a3eb021a9bcce9dc2c4010cc8
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-12-07 21:39:34 +0000

    ex04

---
+++ ex03.py
@@ -0,0 +1,4 @@
+Dia = input("Qual o dia do seu aniversário? ")
+Mes = input("Qual é o ano do seu aniversário? ")
+Ano = input("Qual é o mês do seu aniversário? ")
+print(Dia, "/", Mes,"/", Ano)
---
+++ ex04.py
@@ -0,0 +1,6 @@
+num1 = input("Escreva o primeiro número: ")
+num2 = input("Escreva o segundo número: ")
+
+sum = int(num1)+int(num2);
+
+print(f"Resultado da soma:{sum}")
