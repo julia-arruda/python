11e7c87b29d1c38b1d5bf92b117841f89dc0fd89
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-12-07 22:03:40 +0000

    Teste de tipagem de variavél

---
+++ ex06.py
@@ -0,0 +1,4 @@
+#Tipos de dados int, float, bool, str
+ler = input("Me dê um dado para fazer um teste do que ele é: ")
+print("É um número? ", ler.isnumeric())
+print("É uma string? ", ler.isalpha())
