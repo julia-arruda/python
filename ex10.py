2f42a46e92e9f4d0fcba4f0467d38748b4c730d5
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-12-07 23:22:48 +0000

    exercicio desconto

---
+++ ex10.py
@@ -0,0 +1,3 @@
+num=float(input("Qual o valor você tem em reais: "))
+dolares=num/5.44
+print(f"O valor em doláres é: {dolares}")
---
+++ ex11.py
@@ -0,0 +1,3 @@
+produto = float(input("Informe o valor do produto com 5% de desconto"))
+desconto=produto-(produto*0.05);
+print(f"O valor do produto com desconto: {desconto} ")
