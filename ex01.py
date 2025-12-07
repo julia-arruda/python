d605afe5360b5dcba7ba9cdb8b29e5839d97756a
Julia Arruda <93848683+julia-arruda@users.noreply.github.com>
2025-12-07 21:28:56 +0000

    exercicio1 python

---
+++ ex01.py
@@ -0,0 +1,5 @@
+peso = input("Qual é o seu peso? ")
+idade = input("Qual é a sua idade? ")
+nome = input("Qual é o seu nome? ")
+
+print("O meu nome é ", nome)
---
+++ script.py
@@ -0,0 +1,4 @@
+print('Python', 8)
+nome = 'Julia'
+idade=24
+print(nome, idade)
